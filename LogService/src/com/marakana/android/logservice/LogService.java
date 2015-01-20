2fa61e2df5eee8246c04693b8af23bfb1e5f6894
Dave Smith <smith@wiresareobsolete.com>
2015-01-20 16:42:01 -0700

    Fix remaining merge issues.

--- LogService/src/com/marakana/android/logservice/LogService.java
+++ LogService/src/com/marakana/android/logservice/LogService.java
@@ -3,31 +3,34 @@ package com.marakana.android.logservice;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class LogService extends Service {
 	private static final String TAG = "LogService";
-	
-	/* BINDER LAB: We'll need a reference to our Binder service */
+	private ILogServiceImpl service;
 
 	@Override
 	public void onCreate() {
+		Log.d(TAG, "onCreate()'d");
 		super.onCreate();
-		this.service = new ILogServiceImpl(super.getApplicationContext());
+		this.service = new ILogServiceImpl();
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		/* BINDER LAB: Modify this to return a reference to the Binder service */
-		return null;
+		Log.d(TAG, "onBind()'d");
+		return this.service;
 	}
 
 	@Override
 	public boolean onUnbind(Intent intent) {
+		Log.d(TAG, "onUnbind()'d");
 		return super.onUnbind(intent);
 	}
 
 	@Override
 	public void onDestroy() {
+		Log.d(TAG, "onDestroy()'d");
 		super.onDestroy();
 	}
 }
